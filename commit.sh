9104b37f4c9fa5384ffc2523712f13d61786e69f
Cygra <sjtuwbh@gmail.com>
2018-12-27 11:14:22 +0800

    "满园花菊郁金黄，中有孤丛色似霜。"
    ✨ add commit.sh

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 使用一言 API （https://gushi.ci/）随机获取诗词作为 commit header。
+
+commit=$1
+msg=$( curl 'https://api.gushi.ci/all.json' | jq '.content' )
+git add .
+git commit -m "
+${msg}
+
+${commit}
+"
