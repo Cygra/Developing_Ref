1b62fb65117063ae991f2027200c94832c248d58
Cygra Wang <sjtuwbh@gmail.com>
2021-01-15 10:41:38 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 now="$(date +'%Y%m%d%H%M')"
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-ENV=""
+env=""
 
 case $current_branch in
-develop) ENV="dev" ;;
-rc) ENV="rc" ;;
-master) ENV="prod" ;;
+develop) env="dev" ;;
+rc) env="rc" ;;
+master) env="prod" ;;
 *) echo "\033[31m 不在 develop|rc|master 分支 \033[0m" && exit ;;
 esac
 
-tag="deploys/$ENV/$now"
+tag="deploys/$env/$now"
 git tag $tag
 git push origin $tag
