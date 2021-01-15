2daaaf13c9737153842b35c536128d75081159c9
Cygra Wang <sjtuwbh@gmail.com>
2021-01-15 10:43:59 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 # alias ddd="~/deploy.sh"
 
-now="$(date +'%Y%m%d%H%M')"
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 env=""
 
@@ -11,6 +10,7 @@ master) env="prod" ;;
 *) echo "\033[31m 不在 develop|rc|master 分支 \033[0m" && exit ;;
 esac
 
+now="$(date +'%Y%m%d%H%M')"
 tag="deploys/$env/$now"
 git tag $tag
 git push origin $tag
